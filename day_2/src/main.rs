d0e29bc8bc9c9b765278987f68468b5dba7aa9b3
Samuel Golden <samgolden00@gmail.com>
2023-12-02 18:18:59 -0500

    day 2 part 1

---
+++ day_2/src/main.rs
@@ -0,0 +1,74 @@
+use std::io::{stdin, BufRead, BufReader};
+
+#[derive(Debug)]
+struct Game {
+    id: u32,
+    r: Vec<u32>,
+    g: Vec<u32>,
+    b: Vec<u32>,
+}
+
+fn main() {
+    let check_values = (12u32, 13u32, 14u32);
+
+    let lines = BufReader::new(stdin().lock()).lines();
+    let games: Vec<Game> = lines
+        .filter_map(|l| match l {
+            Ok(s) => Some(parse_line(s)),
+            Err(_) => None,
+        })
+        .collect();
+
+    let answer: u32 = games
+        .iter()
+        .filter_map(|game| {
+            if game.r.iter().max().unwrap_or(&0) > &check_values.0
+                || game.g.iter().max().unwrap_or(&0) > &check_values.1
+                || game.b.iter().max().unwrap_or(&0) > &check_values.2
+            {
+                return None;
+            }
+            Some(game.id)
+        })
+        .sum();
+
+    println!("{}", answer);
+}
+
+fn parse_line(line: String) -> Game {
+    let mut game = Game {
+        id: 0u32,
+        r: vec![],
+        g: vec![],
+        b: vec![],
+    };
+
+    let mut iter = line.split(':');
+
+    // get game id
+    game.id = match iter.next() {
+        Some(s) => s.replace("Game ", "").parse::<u32>().unwrap(),
+        None => panic!(),
+    };
+
+    // get cube counts
+    iter.next()
+        .unwrap_or_default()
+        .split(';')
+        .for_each(|round| {
+            // get the colors in each round
+            round.split(',').map(str::trim).for_each(|color| {
+                // separate num and color
+                let mut inner_iter = color.split(' ');
+                let num = inner_iter.next().unwrap().parse::<u32>().unwrap();
+                match inner_iter.next().unwrap() {
+                    "red" => game.r.push(num),
+                    "green" => game.g.push(num),
+                    "blue" => game.b.push(num),
+                    &_ => panic!(),
+                }
+            });
+        });
+
+    return game;
+}
