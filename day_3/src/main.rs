333553615a3ece6e2e81e068879fde3f697f0ea4
Samuel Golden <samgolden00@gmail.com>
2023-12-03 15:04:21 -0500

    day 3 part 2

--- day_3/src/main.rs
+++ day_3/src/main.rs
@@ -1,4 +1,4 @@
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 use std::fmt::Write;
 use std::io::{stdin, BufRead, BufReader};
 use std::iter;
@@ -27,7 +27,8 @@ fn main() {
     let input = BufReader::new(stdin().lock()).lines();
     let lines: Vec<Result<String, std::io::Error>> = input.collect();
 
-    let mut symbol_map: HashMap<Point, bool> = HashMap::new();
+    let mut gear_counter: u32 = 0;
+    let mut gear_map: HashMap<Point, HashSet<u32>> = HashMap::new();
 
     // Vec<(start, end, value)>
     let mut numbers: Vec<(Point, Point, String)> = vec![];
@@ -48,37 +49,63 @@ fn main() {
                         }
                     }
                     numbers.push((Point { x, y }, Point { x, y }, char.to_string()));
-                } else if char != '.' {
-                    // for each point surrounding the symbol, add flag to map
+                } else if char == '*' {
+                    gear_counter += 1;
+                    // for each point surrounding the gear, add the gear's id to the map
                     AREA_DELTAS.iter().for_each(|(dx, dy)| {
-                        symbol_map.insert(
-                            Point {
-                                x: x + dx,
-                                y: y + dy,
-                            },
-                            true,
-                        );
+                        let p = Point {
+                            x: x + dx,
+                            y: y + dy,
+                        };
+                        match gear_map.get_mut(&p) {
+                            Some(gear_set) => drop(gear_set.insert(gear_counter)),
+                            None => drop(gear_map.insert(p, HashSet::from([gear_counter]))),
+                        };
                     });
                 }
             }
         }
     }
 
+    let mut gear_hits: HashMap<u32, HashSet<u32>> = HashMap::new();
+
     // check every point between the start and end of each number for a corresponding
     // flag in the symbol map; sum those with a hit
-    let sum: u32 = numbers
-        .iter()
-        .filter_map(|(start, end, val)| {
-            if (start.x..=end.x)
-                .zip(iter::repeat(start.y))
-                .map(|(x, y)| Point { x, y })
-                .any(|p| *symbol_map.get(&p).unwrap_or(&false))
-            {
-                return Some(val);
+    numbers.iter().for_each(|(start, end, val)| {
+        (start.x..=end.x)
+            .zip(iter::repeat(start.y))
+            .map(|(x, y)| Point { x, y })
+            // for each point in the boundaries of the number, check which gears are adjacent in the gear map
+            .for_each(|p| match gear_map.get(&p) {
+                // if there are adjacent gears, add the value of the number to the gear_hits map for each adjacent gear
+                Some(adj_gears) => {
+                    adj_gears
+                        .iter()
+                        .for_each(|adj_gear| match gear_hits.get_mut(adj_gear) {
+                            Some(ratio_list) => {
+                                let _ = ratio_list.insert(val.parse::<u32>().unwrap());
+                            }
+                            None => {
+                                drop(gear_hits.insert(
+                                    *adj_gear,
+                                    HashSet::from([val.parse::<u32>().unwrap()]),
+                                ))
+                            }
+                        })
+                }
+                None => (),
+            })
+    });
+
+    let sum: u32 = gear_hits
+        .values()
+        .filter_map(|ratios| match ratios.len() {
+            2 => {
+                let rs = ratios.iter().collect::<Vec<&u32>>();
+                Some(rs[0] * rs[1])
             }
-            None
+            _ => None,
         })
-        .map(|val| val.parse::<u32>().unwrap())
         .sum();
 
     println!("{:?}", sum);
