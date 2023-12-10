d6d8b9cf7e4e5bd03f32661065dff77ce8c055ec
Samuel Golden <samgolden00@gmail.com>
2023-12-10 02:14:19 -0500

    day 8 part 2

--- day_8/src/main.rs
+++ day_8/src/main.rs
@@ -1,4 +1,5 @@
 use itertools::Itertools;
+use num_integer::Integer;
 use std::{
     collections::HashMap,
     io::{stdin, BufRead, BufReader},
@@ -9,7 +10,9 @@ fn main() {
 
     let turns: String = lines.next().expect("first line").expect("read line");
     let mut map: HashMap<String, (String, String)> = HashMap::new();
+    let mut tracked_nodes: Vec<String> = vec![];
 
+    // build map and identify starting nodes
     lines.skip(1).for_each(|l| match l {
         Ok(line) => {
             let mut iter = line.split(" = ");
@@ -27,22 +30,45 @@ fn main() {
                 .expect("build tuple");
 
             map.insert(key.to_string(), conn_tup);
+            if key.ends_with('A') {
+                tracked_nodes.push(key.to_string());
+            }
         }
         Err(_) => panic!(),
     });
 
-    let mut count = 0u32;
-    let mut curr: &String = &String::from("AAA");
-    for turn in turns.chars().cycle() {
-        if curr == "ZZZ" {
-            println!("turn count: {}", count);
-            break;
+    println!("starting nodes: {:?}", tracked_nodes);
+
+    // count the turns to the first z node for each node that ends with a
+    let mut first_hits: Vec<u64> = vec![];
+    for node in tracked_nodes.iter_mut() {
+        let mut count = 0;
+        for turn in turns.chars().cycle() {
+            *node = match turn {
+                'L' => map
+                    .get(node)
+                    .expect("current location in map")
+                    .0
+                    .to_string(),
+                'R' => map
+                    .get(node)
+                    .expect("current location in map")
+                    .1
+                    .to_string(),
+                _ => panic!(),
+            };
+            count += 1;
+            if node.chars().last().expect("get last char") == 'Z' {
+                println!("{}: {}", node, count);
+                first_hits.push(count);
+                break;
+            }
         }
-        curr = match turn {
-            'L' => &map.get(curr).expect("current location in map").0,
-            'R' => &map.get(curr).expect("current location in map").1,
-            _ => panic!(),
-        };
-        count += 1;
     }
+
+    // find the least common multiple of all the counts
+    println!(
+        "lcm of all: {}",
+        first_hits.iter().fold(1i64, |acc, i| acc.lcm(&(*i as i64)))
+    );
 }
