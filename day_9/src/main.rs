4a647f91cf29119f5703167934d69eafee5a8dc2
Samuel Golden <samgolden00@gmail.com>
2023-12-10 18:01:24 -0500

    day 9 part 1

---
+++ day_9/src/main.rs
@@ -0,0 +1,28 @@
+use std::io::{stdin, BufRead, BufReader};
+
+use sequence::{Int, Sequence};
+mod sequence;
+
+fn main() {
+    let lines = BufReader::new(stdin().lock()).lines();
+    let mut seqs: Vec<Sequence> = vec![];
+    lines.for_each(|line| {
+        seqs.push(Sequence::new(
+            line.expect("read line")
+                .split(' ')
+                .filter_map(|n| n.parse::<Int>().ok())
+                .collect(),
+        ))
+    });
+
+    let next_vals_sum: Int = seqs
+        .iter_mut()
+        .map(|s| {
+            let next = s.extrapolate_next();
+            //println!("{}\n", s);
+            next
+        })
+        .sum();
+
+    println!("answer: {}", next_vals_sum);
+}
---
+++ day_9/src/sequence.rs
@@ -0,0 +1,64 @@
+use itertools::Itertools;
+use std::fmt::Display;
+pub type Int = i64;
+
+pub struct Sequence {
+    sequences: Vec<Vec<Int>>,
+    seq_final_vals: Vec<Int>,
+    zero_layer_built: bool,
+}
+
+impl Sequence {
+    pub fn new(values: Vec<Int>) -> Self {
+        Self {
+            zero_layer_built: !values.iter().any(|x| *x != 0),
+            seq_final_vals: vec![*values.last().expect("has value")],
+            sequences: vec![values],
+        }
+    }
+
+    fn next_layer(&mut self) {
+        if self.zero_layer_built {
+            println!("zero layer already built!");
+            return;
+        }
+        let mut new_layer: Vec<Int> = vec![];
+        let mut all_zero = true;
+        self.sequences
+            .last()
+            .expect("has 1 seq")
+            .as_slice()
+            .windows(2)
+            .for_each(|win| {
+                let diff = win[1] - win[0];
+                if diff != 0 {
+                    all_zero = false;
+                }
+                new_layer.push(diff);
+            });
+        self.zero_layer_built = all_zero;
+        self.seq_final_vals
+            .push(*new_layer.last().expect("has a value"));
+        self.sequences.push(new_layer);
+    }
+
+    pub fn extrapolate_next(&mut self) -> Int {
+        while !self.zero_layer_built {
+            self.next_layer();
+        }
+        return self.seq_final_vals.iter().rev().fold(0, |acc, n| acc + n);
+    }
+}
+
+impl Display for Sequence {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "{}",
+            self.sequences
+                .iter()
+                .map(|seq| seq.iter().join("\t"))
+                .join("\n")
+        )
+    }
+}
